268b99df668bf4d39381ddb39237ba9ba801fb81
Dmitry <dmitryneposidjaka@gmail.com>
2020-10-02 01:04:46 +0300

    add Fun, Contacts components

--- src/components/Fun/Fun.tsx
+++ src/components/Fun/Fun.tsx
@@ -4,18 +4,20 @@ import project from 'assets/images/Project-33.png';
 import woodman from 'assets/images/woodman.png';
 import startupVillage from 'assets/images/Startup-Village.png';
 import arrowDown from 'assets/images/icon-arrow-down.svg';
+import arrowUp from 'assets/images/icon-arrow-up.svg';
 
-const Portfolio: React.FC = (): JSX.Element => {
+const Fun: React.FC = (): JSX.Element => {
     return (
-        <section className="full-height" id="portfolio">
+        <section className="full-height" id="fan">
             <div className="full-height-inner">
+                <a href="#portfolio" className="link-to-section up">
+                    <img src={arrowUp} alt="up" />
+                    <span className="link-text">Portfolio</span>
+                </a>
                 <div className="items-wrap">
                     <div className="item">
                         <div className="item__header">
-                            <h5>
-                                Startup Village (Director’s cut) Startup Village (Director’s
-                                cut)
-                            </h5>
+                            <h5>Startup Village (Director’s cut)</h5>
                         </div>
                         <div className="item__image">
                             <img src={startupVillage} alt="Startup Village" />
@@ -51,7 +53,7 @@ const Portfolio: React.FC = (): JSX.Element => {
                             <h5>Startup Village (Director’s cut)</h5>
                         </div>
                         <div className="item__image">
-                            <img src="images/Startup-Village.png" alt="Startup Village" />
+                            <img src={startupVillage} alt="Startup Village" />
                         </div>
                         <div className="item__text">
                             <p>music and sound design for 2d animation</p>
@@ -62,7 +64,7 @@ const Portfolio: React.FC = (): JSX.Element => {
                             <h5>Startup Village (Director’s cut)</h5>
                         </div>
                         <div className="item__image">
-                            <img src="images/Startup-Village.png" alt="Startup Village" />
+                            <img src={startupVillage} alt="Startup Village" />
                         </div>
                         <div className="item__text">
                             <p>music and sound design for 2d animation</p>
@@ -73,7 +75,7 @@ const Portfolio: React.FC = (): JSX.Element => {
                             <h5>Startup Village (Director’s cut)</h5>
                         </div>
                         <div className="item__image">
-                            <img src="images/Startup-Village.png" alt="Startup Village" />
+                            <img src={startupVillage} alt="Startup Village" />
                         </div>
                         <div className="item__text">
                             <p>music and sound design for 2d animation</p>
@@ -84,42 +86,20 @@ const Portfolio: React.FC = (): JSX.Element => {
                             <h5>Startup Village (Director’s cut)</h5>
                         </div>
                         <div className="item__image">
-                            <img src="images/Startup-Village.png" alt="Startup Village" />
-                        </div>
-                        <div className="item__text">
-                            <p>music and sound design for 2d animation</p>
-                        </div>
-                    </div>
-                    <div className="item">
-                        <div className="item__header">
-                            <h5>Startup Village (Director’s cut)</h5>
-                        </div>
-                        <div className="item__image">
-                            <img src="images/Startup-Village.png" alt="Startup Village" />
-                        </div>
-                        <div className="item__text">
-                            <p>music and sound design for 2d animation</p>
-                        </div>
-                    </div>
-                    <div className="item">
-                        <div className="item__header">
-                            <h5>Startup Village (Director’s cut)</h5>
-                        </div>
-                        <div className="item__image">
-                            <img src="images/Startup-Village.png" alt="Startup Village" />
+                            <img src={startupVillage} alt="Startup Village" />
                         </div>
                         <div className="item__text">
                             <p>music and sound design for 2d animation</p>
                         </div>
                     </div>
                 </div>
-                <a href="#fan" className="link-to-section down">
-                    <span className="link-text">FUN</span>
+                <a href="#contacts" className="link-to-section down">
+                    <span className="link-text">contacts</span>
                     <img src={arrowDown} alt="down" />
                 </a>
             </div>
         </section>
     );
-}
+};
 
-export {Portfolio}
+export {Fun}
